bc4d19016faae11a58b15972212bc2379b17d7da
Lamdashi/LQ-84i <155986030+orangejuiceplz@users.noreply.github.com>
2024-10-16 08:12:19 -0400

    Create art.java

---
+++ java/unitsOf1/art.java
@@ -0,0 +1,13 @@
+public class art
+{
+    public static void main(String[] args)
+    {
+        System.out.println("         *   ");
+        System.out.println("      *  *  *");
+        System.out.println("   *  *  *  *  *");
+        System.out.println("*  *  *  *  *  *  *   ");
+        System.out.println("   *  *  *  *  *");
+        System.out.println("      *  *  *");
+        System.out.println("         *   ");
+    }
+}
