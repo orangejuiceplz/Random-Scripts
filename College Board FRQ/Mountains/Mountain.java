28db9d66897114b1e1eb5c2c4f65a725c9eb0c78
LL-LQ-84i <155986030+orangejuiceplz@users.noreply.github.com>
2025-01-31 18:05:33 -0500

    yeah

---
+++ College Board FRQ/Mountains/Mountain.java
@@ -0,0 +1,27 @@
+public class Mountain {
+
+    public static boolean isIncreasing(int[] array, int stop) {
+      // not shown but i'm just going to return true for vscode
+      return true;
+    }
+  
+    public static boolean isDecreasing(int[] array, int start) {
+      // not shown but i'm just going to return true for vscode
+      return true;
+    }
+  
+    public static int getPeakIndex(int[] array) {
+      for (int curPos = 1; curPos < array.length - 1; curPos++) {
+        if (array[curPos] > array[curPos - 1] && 
+            array[curPos] < array[curPos + 1]) {
+          return curPos;
+        }
+      }
+      return -1;
+    }
+  
+    public static boolean isMountain(int[] array) {
+      int peakIndex = getPeakIndex(array);
+      return isIncreasing(array, peakIndex) && isDecreasing(array, peakIndex);
+    }
+}
