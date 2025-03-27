180f8695cafdeae3f521bcef4e2c8154e0562184
λamdashi <155986030+orangejuiceplz@users.noreply.github.com>
2025-03-26 20:25:57 -0400

    Create Sign.java

---
+++ Collegeboard/type/typ2/Sign.java
@@ -0,0 +1,59 @@
+public class Sign {
+// there are some errors so fix
+    private int width;
+
+    private String message;
+
+    public Sign(int width, String message) {
+
+          this.width = width;
+
+          this.message = message;
+    }
+
+    public int numberOfLines() {
+
+         int line = message.length() / width;
+
+         if (message.length() % width > 0) {
+
+                 line++;
+
+          } 
+
+          return line;
+      }
+
+
+
+  public String getLines() {
+
+        if(message.length() == 0) {
+
+             return null;
+
+         }
+
+        String lines = "";
+
+        String remMsg = message;
+
+         while(remMsg.length() > width) {
+
+               lines += remMsg.subString(0, width);
+
+               lines += ";";
+
+               remMsg = remMsg.substring(width);
+
+         }
+
+         lines += remMsg;
+
+         return lines;
+
+      }
+
+}
+
+         
