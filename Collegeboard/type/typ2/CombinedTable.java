04a587325f2ed1557f76b364f3ea0cf3063a326e
λamdashi <155986030+orangejuiceplz@users.noreply.github.com>
2025-03-26 20:26:39 -0400

    Create CombinedTable.java

---
+++ Collegeboard/type/typ2/CombinedTable.java
@@ -0,0 +1,27 @@
+public class CombinedTable {
+	private SingleTable tb1;
+    private SingleTable tb2;
+    
+    public CombinedTable(SingleTable tb1, SingleTable tb2) {
+    	this.tb1 = tb1;
+        this.tb2 = tb2;
+    }
+    
+    // ?
+    
+    public boolean canSeat(int ppl) {
+    	int totalAval = this.tb1.getNumSeats() + this.tb2.getNumSeats() - 2;
+        boolean possible = (totalAval >= ppl);
+        return possible;
+    }
+    
+    // no idea
+    public double getDesirability() {
+    	double avg = (this.tb1.getViewQuality() + this.tb2.getViewQuality()) / 2.0;
+        if (this.tb1.getHeight() == this.tb2.getHeight()) {
+        	return avg;
+        } else {
+        	return avg - 10;
+        }
+    }
+}
